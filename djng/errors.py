f84e699b0cc756ce8c4ef136a903a08d4ab1e1ba
Ivan Cherevko <i@cherevko.net>
2011-09-07 10:02:17 +0300

    Solving issue #1
    As per ssadler's suggestions.

--- djng/errors.py
+++ djng/errors.py
@@ -5,7 +5,7 @@ class ErrorWrapper(object):
     def __init__(self, app, custom_404 = None, custom_500 = None):
         self.app = app
         self.error_404 = custom_404 or self.default_error_404
-        self.error_500 = custom_500 or self.default_error_404
+        self.error_500 = custom_500 or self.default_error_500
     
     def __call__(self, request):
         try:
@@ -20,4 +20,4 @@ class ErrorWrapper(object):
         return Response('A 404 error occurred', status=404)
     
     def default_error_500(self, request, e):
-        return Response('A 500 error occurred: %r' % e, status=505)
+        return Response('A 500 error occurred: %r' % e, status=500)
